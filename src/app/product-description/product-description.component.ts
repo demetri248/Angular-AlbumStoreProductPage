2c808e6af0b555176ed7623a4bc947ab83cb61d7
demetri248 <dlk212@gmail.com>
2018-09-11 22:23:35 -0500

    part 3.2

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -11,6 +11,7 @@ export class ProductDescriptionComponent implements OnInit {
 albumInfo;
 
 
+
   constructor(private _productService: ProductService) { }
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
