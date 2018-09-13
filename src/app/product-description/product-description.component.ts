575145088f2f9a0d385af602117565e43e0c7a7b
demetri248 <dlk212@gmail.com>
2018-09-12 21:29:11 -0500

    part 4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
@@ -8,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-albumInfo;
+albumInfo: Album;
 
 
 
